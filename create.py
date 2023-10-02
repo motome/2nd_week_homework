57db031a06b661fe939f24458a671d5084e63401
motome <sigmapinefield15@gmail.com>
2023-10-02 16:46:50 +0900

    4th commit formula create_Customer_list defined

--- create.py
+++ create.py
@@ -1,6 +1,10 @@
 from db_config import Customer_list
 
 # インスタンスを生成しないでcreate
-Customer_list.create(customer="Bob", age=15)
-Customer_list.create(customer="Tom", age=57)
-Customer_list.create(customer="Ken", age=73)
+def create_Customer_list():
+    Customer_list.create(customer="Bob", age=15)
+    Customer_list.create(customer="Tom", age=57)
+    Customer_list.create(customer="Ken", age=73)
+
+if __name__ == "__main__":
+    create_Customer_list()
