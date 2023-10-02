d1a9008f7eadb0a165994d9bb34961018b60dcbb
motome <sigmapinefield15@gmail.com>
2023-10-02 16:38:56 +0900

    3rd commit records wrote in the table

---
+++ create.py
@@ -0,0 +1,6 @@
+from db_config import Customer_list
+
+# インスタンスを生成しないでcreate
+Customer_list.create(customer="Bob", age=15)
+Customer_list.create(customer="Tom", age=57)
+Customer_list.create(customer="Ken", age=73)
