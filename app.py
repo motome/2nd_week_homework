2dc4b64ac9445e8b9c24838dd2fd15d207941594
motome <sigmapinefield15@gmail.com>
2023-10-03 11:06:37 +0900

    6th commit head comment finished

---
+++ app.py
@@ -0,0 +1,8 @@
+print("===== Welcome to CRM Application =====")
+print("[S]how: Show all users info")
+print("[A]dd: Add new user")
+print("[Q]uit: Quit The Application")
+print("======================================")
+
+
+
